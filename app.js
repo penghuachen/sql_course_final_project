cfb7b65a0bdeff8485578db616759420c02754a4
penghuachen <kids5346@gmail.com>
2022-10-12 11:35:08 +0800

    style: remove require database in app.js

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-const db = require("./database/mysql.js");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
